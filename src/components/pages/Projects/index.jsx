bf50ef90b2442849420b80b20f0c54fc8e2ba603
unknown <thomasjjohnson9311@gmail.com>
2023-06-08 01:14:24 -0500

    cards to project page, & grid columns

--- src/components/pages/Projects/index.jsx
+++ src/components/pages/Projects/index.jsx
@@ -6,8 +6,91 @@ import { SeeSourceCodeForSVG } from "flowbite-react";
 
 export default function Projects() {
   return (
-    <div>
-      <Button className="bg-red-500 hover:bg-red-600">Click me</Button>;
+    <div className="grid grid-cols-2">
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="../IMG_E0070.JPG"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="../IMG_E0070.JPG"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="../IMG_E0070.JPG"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="../IMG_E0070.JPG"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="../IMG_E0070.JPG"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
+      <Card
+        imgAlt="Meaningful alt text for an image that is not purely decorative"
+        imgSrc="../IMG_E0070.JPG"
+      >
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
-  darkMode: "media",
+  // darkMode: "media",
   theme: {
     extend: {},
   },
