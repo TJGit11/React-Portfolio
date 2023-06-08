8c9f7dc8e28e5b621e7da61bf891aec293172863
unknown <thomasjjohnson9311@gmail.com>
2023-06-08 01:38:11 -0500

    playing with style ideas on about & contact pages before moving to color theme

--- src/components/pages/Contact/index.jsx
+++ src/components/pages/Contact/index.jsx
@@ -1,9 +1,20 @@
 import React from "react";
+import { Card } from "flowbite-react";
 
 export default function Contact() {
   return (
     <div>
-      <h1>Contact Page</h1>
+      <Card href="#">
+        <h5 className="text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          <p>Noteworthy technology acquisitions 2021</p>
+        </h5>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          <p>
+            Here are the biggest enterprise technology acquisitions of 2021 so
+            far, in reverse chronological order.
+          </p>
+        </p>
+      </Card>
     </div>
   );
 }
