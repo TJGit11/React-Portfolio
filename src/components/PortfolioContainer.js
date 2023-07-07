81e36d412f1f7763b82aa1c4a231e6b089c68ab3
unknown <thomasjjohnson9311@gmail.com>
2023-07-06 23:48:20 -0500

    removed blog import in portfoliocontainer

--- src/components/PortfolioContainer.js
+++ src/components/PortfolioContainer.js
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import NavTabs from "./NavTabs";
 import Home from "./pages/Home";
 import About from "./pages/About";
-import Blog from "./pages/Blog";
 import Contact from "./pages/Contact";
 import Projects from "./pages/Projects";
 import Footer from "./Footer";
