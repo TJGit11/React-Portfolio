b63d224f592714cc281fcfa17b1a225ff815f96a
unknown <thomasjjohnson9311@gmail.com>
2023-07-06 23:50:59 -0500

    cleaned up code in navtabs

--- src/components/NavTabs.js
+++ src/components/NavTabs.js
@@ -4,16 +4,7 @@ import { Navbar } from "flowbite-react";
 function NavTabs({ currentPage, handlePageChange }) {
   return (
     <Navbar fluid rounded>
-      <Navbar.Brand>
-        {/* <img
-          alt="Flowbite React Logo"
-          className="mr-3 h-6 sm:h-9"
-          src="/favicon.svg"
-        /> */}
-        {/* <span className="self-center whitespace-nowrap text-xl font-semibold text-black">
-          Thomas Johnson
-        </span> */}
-      </Navbar.Brand>
+      <Navbar.Brand></Navbar.Brand>
       <Navbar.Toggle />
       <Navbar.Collapse>
         <Navbar.Link
@@ -55,65 +46,6 @@ function NavTabs({ currentPage, handlePageChange }) {
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
-
-    // <ul className="nav nav-tabs bg-[#ecfdf5]">
-    //   {/* <li className="nav-item">
-    //     <a
-    //       href="#home"
-    //       onClick={() => handlePageChange("Home")}
-    //       className={currentPage === "Home" ? "nav-link active" : "nav-link"}
-    //     >
-    //       Home
-    //     </a>
-    //   </li> */}
-    //   <li className="nav-item">
-    //     <a
-    //       href="#about"
-    //       onClick={() => handlePageChange("About")}
-    //       className={currentPage === "About" ? "nav-link active" : "nav-link"}
-    //     >
-    //       About
-    //     </a>
-    //   </li>
-    //   <li className="nav-item">
-    //     <a
-    //       href="#projects"
-    //       onClick={() => handlePageChange("Projects")}
-    //       className={
-    //         currentPage === "Projects" ? "nav-link active" : "nav-link"
-    //       }
-    //     >
-    //       Projects
-    //     </a>
-    //   </li>
-    //   {/* <li className="nav-item">
-    //     <a
-    //       href="#about"
-    //       onClick={() => handlePageChange("About")}
-    //       className={currentPage === "About" ? "nav-link active" : "nav-link"}
-    //     >
-    //       About
-    //     </a>
-    //   </li> */}
-    //   <li className="nav-item">
-    //     <a
-    //       href="#blog"
-    //       onClick={() => handlePageChange("Blog")}
-    //       className={currentPage === "Blog" ? "nav-link active" : "nav-link"}
-    //     >
-    //       Resume
-    //     </a>
-    //   </li>
-    //   <li className="nav-item">
-    //     <a
-    //       href="#contact"
-    //       onClick={() => handlePageChange("Contact")}
-    //       className={currentPage === "Contact" ? "nav-link active" : "nav-link"}
-    //     >
-    //       Contact
-    //     </a>
-    //   </li>
-    // </ul>
   );
 }
 
