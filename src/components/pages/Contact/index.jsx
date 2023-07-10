537d1a48e562c1a7ee4166e8c26651041041f5ad
unknown <thomasjjohnson9311@gmail.com>
2023-07-10 18:22:02 -0500

    github link now opens in a new tab instead of the same one

--- src/components/pages/Contact/index.jsx
+++ src/components/pages/Contact/index.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Card } from "flowbite-react";
 import { BsLinkedin } from "react-icons/bs";
 import { BsGithub } from "react-icons/bs";
 import { MdMail } from "react-icons/md";
@@ -14,12 +13,16 @@ export default function Contact() {
       </div>
       <div className="border border-black p-20 m-5">
         <span className="flex justify-center">Github</span>
-        <a href="#" alt="A link to Thomas Johnson's github page">
+        <a
+          href="https://www.github.com/TJGit11"
+          alt="A link to Thomas Johnson's github page"
+          target="_blank"
+        >
           <BsGithub size="4em"></BsGithub>
         </a>
       </div>
       <div className="border border-black p-20 m-5">
-        <span className="flex justify-center">Gmail</span>
+        <span className="flex justify-center">Email</span>
         <a
           href="mailto: thomasjjohnson9311@gmail.com"
           alt="A link to send Thomas Johnson an email"
@@ -27,67 +30,6 @@ export default function Contact() {
           <MdMail size="4em"></MdMail>
         </a>
       </div>
-      {/* <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border border-primary">
-        <div className="card-body">
-          <h1 className="text-1xl mb-4">
-            Please submit this form below to leave us a message!
-          </h1>
-          <form onSubmit>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Full Name</span>
-              </label>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered"
-                value
-                onChange={(event) => event.target.value}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered"
-                value
-                onChange={(event) => event.target.value}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Phone Number</span>
-              </label>
-              <input
-                type="text"
-                placeholder=""
-                className="input input-bordered"
-                value
-                onChange={(event) => event.target.value}
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Message</span>
-              </label>
-              <textarea
-                placeholder=""
-                className="textarea textarea-bordered"
-                value
-                onChange={(event) => event.target.value}
-              ></textarea>
-            </div>
-            <div className="form-control mt-6">
-              <button type="submit" className="btn btn-primary">
-                Submit
-              </button>
-            </div>
-          </form>
-        </div>
-      </div> */}
     </div>
   );
 }
