96040ac95963953c1bd5f882fb9a816f2d06fd7d
unknown <thomasjjohnson9311@gmail.com>
2023-07-10 18:18:14 -0500

    fixed icon position & added mailto link to email option

--- src/components/pages/Contact/index.jsx
+++ src/components/pages/Contact/index.jsx
@@ -6,21 +6,26 @@ import { MdMail } from "react-icons/md";
 export default function Contact() {
   return (
     <div className="flex flex-wrap items-center justify-center p-20">
-      <a href="#" alt="A link to Thomas Johnson's LinkedIn page">
-        <div className="border border-black p-20">
-          <span>LinkedIn</span>
+      <div className="border border-black p-20 m-5">
+        <span className="flex justify-center">LinkedIn</span>
+        <a href="#" mailto alt="A link to Thomas Johnson's LinkedIn page">
           <BsLinkedin size="4em"></BsLinkedin>
-        </div>
-      </a>
-      <div className="border border-black p-20">
+        </a>
+      </div>
+      <div className="border border-black p-20 m-5">
+        <span className="flex justify-center">Github</span>
         <a href="#" alt="A link to Thomas Johnson's github page">
-          <span>Github</span>
           <BsGithub size="4em"></BsGithub>
         </a>
       </div>
-      <div className="border border-black p-20">
-        <MdMail size="4em"></MdMail>
-        <a href="#" alt="A link to send Thomas Johnson an email"></a>
+      <div className="border border-black p-20 m-5">
+        <span className="flex justify-center">Gmail</span>
+        <a
+          href="mailto: thomasjjohnson9311@gmail.com"
+          alt="A link to send Thomas Johnson an email"
+        >
+          <MdMail size="4em"></MdMail>
+        </a>
       </div>
       {/* <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 border border-primary">
         <div className="card-body">
